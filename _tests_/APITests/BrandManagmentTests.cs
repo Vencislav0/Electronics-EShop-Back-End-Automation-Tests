8b02b5c640be2055eba8659ffdf1ef9eea2e483e
Vencislav0 <darkslayer629@gmail.com>
2024-12-11 21:52:24 +0000

    added e2e test for product with coupon applied order then cancelling the order

---
+++ _tests_/APITests/BrandManagmentTests.cs
@@ -0,0 +1,39 @@
+﻿using ClassLibrary;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APITests
+{
+    public  class BrandManagmentTests
+    {
+        private RestClient client;
+        private string adminToken;
+        private string userToken;
+        private Random random;
+
+        [SetUp]
+        public void SetUp()
+        {
+            client = new RestClient(GlobalConstants.BaseUrl);
+            adminToken = GlobalConstants.AuthenticateUser("admin@gmail.com", "admin@gmail.com");
+            userToken = GlobalConstants.AuthenticateUser("johndoe@example.com", "password123");
+            random = new Random();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            client.Dispose();
+        }
+
+        
+
+
+    }
+}
--- _tests_/APITests/CouponManagmentTests.cs
+++ _tests_/APITests/CouponManagmentTests.cs
@@ -101,7 +101,7 @@ namespace APITests
             var productPrice1 = products.Where(p => p["_id"]?.ToString() == firstID).Select(p => p["price"]?.ToString()).FirstOrDefault();
             var productPrice2 = products.Where(p => p["_id"]?.ToString() == secondID).Select(p => p["price"]?.ToString()).FirstOrDefault();
 
-            Assert.That(applyCouponResponse.IsSuccessful, Is.True, $"Unable to add coupon items {applyCouponResponse.StatusCode}");
+            Assert.That(applyCouponResponse.IsSuccessful, Is.True, $"Unable to apply coupon {applyCouponResponse.StatusCode}");
 
 
             //Delete the created coupon
@@ -180,12 +180,92 @@ namespace APITests
 
             var placeOrderRequest = new RestRequest("/user/cart/cash-order", Method.Post);
             placeOrderRequest.AddHeader("Authorization", $"Bearer {userToken}");
-            placeOrderRequest.AddJsonBody(JsonConvert.SerializeObject(new { COD = true, couponApplied = false }));
+            placeOrderRequest.AddJsonBody(JsonConvert.SerializeObject(new { COD = true, couponApplied = true }));
 
             var placeOrderResponse = client.Execute(placeOrderRequest);
 
             Assert.That(placeOrderResponse.IsSuccessful, Is.True, $"Unable to place order {cartResponse.StatusCode}");
             Assert.That(JObject.Parse(placeOrderResponse.Content)["message"]?.ToString(), Is.EqualTo("success"));
+
+
         }
+
+
+        [Test]
+        public void ComplexOrderLifeCycleTest()
+        {
+            //Get all products
+            var getAllProductsRequest = new RestRequest("/product/", Method.Get);
+            getAllProductsRequest.AddHeader("Authorization", $"Bearer {adminToken}");
+
+            var getAllProductsResponse = client.Execute(getAllProductsRequest);
+
+            var products = JArray.Parse(getAllProductsResponse.Content);
+
+            Assert.That(getAllProductsResponse.IsSuccessful, Is.True, $"Getting products failed {getAllProductsResponse.StatusCode}");
+
+            //Getting the first product id
+            var firstProductId = products.First()["_id"]?.ToString();
+
+            //Adding the product to cart
+            var cartRequest = new RestRequest("/user/cart", Method.Post);
+            cartRequest.AddHeader("Authorization", $"Bearer {userToken}");
+            cartRequest.AddJsonBody(new
+            {
+                cart = new[]
+                {
+                    new { _id = firstProductId, count = 1, color = "red" }
+
+                }
+
+            });
+
+            var cartResponse = client.Execute(cartRequest);
+
+            Assert.That(cartResponse.IsSuccessful, Is.True, $"Unable to add items to cart {cartResponse.StatusCode}");
+
+            //Applying coupon
+            var applyCouponRequest = new RestRequest("/user/cart/applycoupon", Method.Post);
+            applyCouponRequest.AddHeader("Authorization", $"Bearer {userToken}");
+            applyCouponRequest.AddJsonBody(new { coupon = "BLACKFRIDAY" });
+
+            var applyCouponResponse = client.Execute(applyCouponRequest);
+
+            Assert.That(applyCouponResponse.IsSuccessful, Is.True, $"Unable to apply coupon {applyCouponResponse.StatusCode}");
+
+            //Place the order with the coupon
+            var placeOrderRequest = new RestRequest("/user/cart/cash-order", Method.Post);
+            placeOrderRequest.AddHeader("Authorization", $"Bearer {userToken}");
+            placeOrderRequest.AddJsonBody(JsonConvert.SerializeObject(new { COD = true, couponApplied = true }));
+
+            var placeOrderResponse = client.Execute(placeOrderRequest);
+
+            Assert.That(placeOrderResponse.IsSuccessful, Is.True, $"Unable to place order {cartResponse.StatusCode}");
+            Assert.That(JObject.Parse(placeOrderResponse.Content)["message"]?.ToString(), Is.EqualTo("success"));
+
+            //Get all user orders
+            var getUserOrdersRequest = new RestRequest("/user/get-orders", Method.Get);
+            getUserOrdersRequest.AddHeader("Authorization", $"Bearer {userToken}");
+
+            var getUserOrdersResponse = client.Execute(getUserOrdersRequest);
+
+            Assert.That(getUserOrdersResponse.IsSuccessful, Is.True, $"Unable to get user orders {cartResponse.StatusCode}");
+
+            //Get order ID
+            var orderId = JObject.Parse(getUserOrdersResponse.Content)["_id"]?.ToString();
+
+            //Update the order and cancel it
+            var updateOrderRequest = new RestRequest("/user/order/update-order/" + orderId, Method.Put);
+            updateOrderRequest.AddHeader("Authorization", $"Bearer {adminToken}");
+            updateOrderRequest.AddJsonBody(new { status = "Cancelled" });
+
+            var updateOrderResponse = client.Execute(updateOrderRequest);
+
+            Assert.That(updateOrderResponse.IsSuccessful, Is.True, $"Unable to update the order status {cartResponse.StatusCode}");
+
+        }
+
+
     }
 }
+
